eebb703e2c35274558b3ce8b64cbf93b81e70202
Enes Ercan Enwar Hurmuzlu <enesalhurmuzi@gmail.com>
2025-02-17 11:51:11 +0300

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Connect to MongoDB
-mongoose.connect("mongodb+srv://blogAdmin:enesadmin@blogcluster.hmrwt.mongodb.net/newsletterDB", {
+mongoose.connect(, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => console.log("MongoDB Connected"))
